a934b9c2919904bae07f74f7b2d49baf7665c0d2
QuantumGorilla <manotasja@uninorte.edu.co>
2020-11-03 19:45:52 -0500

    Funciones para los casos añadidas

--- actividad_4.py
+++ actividad_4.py
@@ -1,7 +1,35 @@
 # Filtrar textos que contengan el error de n seguida de b o n seguida de p y reemplazar por m en ambos casos
-import re
-words = ['abonbar', 'adormecer', 'Acalanbrar', 'Alanbrar', 'acanpar']
-for word in words:
-    e =  re.search('(\w*nb\w*)|(\w*np\w*)', word, flags=re.IGNORECASE)
+import re as regex
+
+def using_regular(input):
+    e = regex.search('(\w*nb\w*)|(\w*np\w*)', input, flags=regex.IGNORECASE)
     if e:
-        print(words.index(e.group()))
+        return e.group()
+        
+
+def sentence_error():
+    sentence = 'Input some shit right here, motherfucker'
+    words = sentence.split(' ')
+    errors = list()
+    for word in words:
+        errors.append(using_regular(word))
+    
+def list_error():
+    words = list()
+    errors = list()
+    for word in words:
+        errors.append(using_regular(word))
+    
+    
+def word_error():
+    word = 'Input'
+    error = using_regular(word)   
+    
+def file_error():
+    name = 'input file name'
+    file = open(name + '.txt')
+    errors = []
+    for line in file:
+        words = line.split(' ')
+        for word in words:
+            errors = using_regular(word)
